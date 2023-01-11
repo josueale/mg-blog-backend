d49afb705bd57c9ff10cd09ca8ef49d857a2b4d4
Josue Ale <jam9814@hotmail.com>
2023-01-11 14:17:55 -0500

    user api updated

--- src/controllers/users/v1/getAllUsers.controller.ts
+++ src/controllers/users/v1/getAllUsers.controller.ts
@@ -1,10 +1,11 @@
+import Users from '@Models/user.model';
 import { Request, Response } from 'express';
 
 export async function getAllUsersControllers(_req: Request, res: Response) {
   try {
+    const allUsers = await Users.find({});
 
-    // users
-
+    res.json(allUsers);
   } catch (error) {
     console.log(error);
     res.status(400).json(error);
